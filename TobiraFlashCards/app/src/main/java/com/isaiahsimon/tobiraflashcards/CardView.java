cba3fabdd63b18977aa101531e5e909b9d1f113a
Isaiah-Simon <zays1993@gmail.com>
2016-02-27 23:06:19 -0500

    Added nextCardBtn Funcionality
    Added functionality for the nextCard button for when an answer is shown.
    This button will then show a new question and reshow the showAnswer
    button.

--- TobiraFlashCards/app/src/main/java/com/isaiahsimon/tobiraflashcards/CardView.java
+++ TobiraFlashCards/app/src/main/java/com/isaiahsimon/tobiraflashcards/CardView.java
@@ -10,7 +10,9 @@ import java.util.Random;
 
 public class CardView extends AppCompatActivity {
     private TextView mCardAnswerTextView;
+    private TextView mCardQuestionTextView;
     private Button mShowCardButton;
+    private Button mNextCard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,22 +20,49 @@ public class CardView extends AppCompatActivity {
         setContentView(R.layout.activity_card_view);
 
         // Array used to test showing data
-        final int[] arr = new int[]{1,2,3,4};
+        final int[] answerArr = new int[]{1,2,3,4};
+        final String[] questionArr = new String[]{"あ","い","え","う"};
 
         mCardAnswerTextView = (TextView) findViewById(R.id.cardAnswerTxtView);
+        mCardQuestionTextView = (TextView) findViewById(R.id.cardQuestionTextView);
         mShowCardButton = (Button) findViewById(R.id.showAnswerBtn);
+        mNextCard = (Button) findViewById(R.id.shownextCard);
 
         View.OnClickListener showAnswer = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Random r = new Random();
-                Integer i = new Integer(r.nextInt(arr.length));
-                String s = new String("This is a string");
+                Integer i = new Integer(r.nextInt(answerArr.length));
+                //Shows answer
+                mCardAnswerTextView.setVisibility(View.VISIBLE);
                 mCardAnswerTextView.setText(i.toString());
+                // Hides show answer button
+                mShowCardButton.setVisibility(View.GONE);
+                //Shows next card button
+                mNextCard.setVisibility(View.VISIBLE);
+            }
+        };
+
+        View.OnClickListener nextCard = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random r = new Random();
+                int i = r.nextInt(questionArr.length);
+                //Hides Answer text view
+                mCardAnswerTextView.setVisibility(View.GONE);
+                //Shows next question
+                mCardQuestionTextView.setVisibility(View.VISIBLE);
+                //Set text of next question
+                mCardQuestionTextView.setText(questionArr[i]);
+                // Shows show answer button
+                mShowCardButton.setVisibility(View.VISIBLE);
+                //Hides next card button
+                mNextCard.setVisibility(View.GONE);
             }
         };
 
         mShowCardButton.setOnClickListener(showAnswer);
+        mNextCard.setOnClickListener(nextCard);
     }
 
 
