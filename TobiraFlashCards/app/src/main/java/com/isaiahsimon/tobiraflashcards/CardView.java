73e1d42f8bb50772f1428afb54836e2ad5371b97
Isaiah-Simon <zays1993@gmail.com>
2016-02-27 22:00:17 -0500

    Added onClick Listener for Button
    Added test listener to cycle through an array and select a random
    integer from the array.

--- TobiraFlashCards/app/src/main/java/com/isaiahsimon/tobiraflashcards/CardView.java
+++ TobiraFlashCards/app/src/main/java/com/isaiahsimon/tobiraflashcards/CardView.java
@@ -2,12 +2,39 @@ package com.isaiahsimon.tobiraflashcards;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class CardView extends AppCompatActivity {
+    private TextView mCardAnswerTextView;
+    private Button mShowCardButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_card_view);
+
+        // Array used to test showing data
+        final int[] arr = new int[]{1,2,3,4};
+
+        mCardAnswerTextView = (TextView) findViewById(R.id.cardAnswerTxtView);
+        mShowCardButton = (Button) findViewById(R.id.showAnswerBtn);
+
+        View.OnClickListener showAnswer = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random r = new Random();
+                Integer i = new Integer(r.nextInt(arr.length));
+                String s = new String("This is a string");
+                mCardAnswerTextView.setText(i.toString());
+            }
+        };
+
+        mShowCardButton.setOnClickListener(showAnswer);
     }
+
+
 }
