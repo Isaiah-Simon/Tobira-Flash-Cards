f2406444185cfca4d0b5d89159cab87bb898a599
Isaiah-Simon <zays1993@gmail.com>
2016-02-29 00:09:28 -0500

    Created Functionality For ListView To Read From Array
    ListView reads from pre-created array of strings.

--- TobiraFlashCards/app/src/main/java/com/isaiahsimon/tobiraflashcards/ListOfDecks.java
+++ TobiraFlashCards/app/src/main/java/com/isaiahsimon/tobiraflashcards/ListOfDecks.java
@@ -2,15 +2,29 @@ package com.isaiahsimon.tobiraflashcards;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 
 public class ListOfDecks extends AppCompatActivity {
     //Adapter used to display list data
-    SimpleCursorAdapter mAdapter;
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_of_decks);
+
+        //Get ListView Object from XML
+        listView = (ListView)findViewById(R.id.deckListView);
+
+        //Create Test array
+        String[] strings = new String[]{"test1","test2","test3"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,
+                android.R.id.text1, strings);
+
+        listView.setAdapter(adapter);
+
     }
 }
