0f02029f069f90fc6157204d3346badbcd9bdcec
Isaiah-Simon <zays1993@gmail.com>
2016-02-28 23:33:50 -0500

    Added Shuffle Onclick listener
    Added onClick listener to shuffle deck and show shuffled deck when
    shuffle button is toggled from off to on. And to show the unshuffled
    deck when turning from on to off.

--- TobiraFlashCards/app/src/main/java/com/isaiahsimon/tobiraflashcards/CardView.java
+++ TobiraFlashCards/app/src/main/java/com/isaiahsimon/tobiraflashcards/CardView.java
@@ -4,9 +4,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Random;
 
 public class CardView extends AppCompatActivity {
@@ -14,6 +17,7 @@ public class CardView extends AppCompatActivity {
     private TextView mCardQuestionTextView;
     private Button mShowCardButton;
     private Button mNextCard;
+    private ToggleButton mToggleButton;
     public int i = 0;
 
     @Override
@@ -31,12 +35,14 @@ public class CardView extends AppCompatActivity {
         deck.add(test1);
         deck.add(test2);
         deck.add(test3);
+        final Deck<Card> shuffleDeck = (Deck<Card>) deck.clone();
 
 
         mCardAnswerTextView = (TextView) findViewById(R.id.cardAnswerTxtView);
         mCardQuestionTextView = (TextView) findViewById(R.id.cardQuestionTextView);
         mShowCardButton = (Button) findViewById(R.id.showAnswerBtn);
         mNextCard = (Button) findViewById(R.id.shownextCard);
+        mToggleButton = (ToggleButton) findViewById(R.id.shuffleToggleButton);
 
         //Show initial card question
         mCardQuestionTextView.setText(deck.get(0).getQuestion());
@@ -44,14 +50,25 @@ public class CardView extends AppCompatActivity {
         View.OnClickListener showAnswer = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Shows answer
-                mCardAnswerTextView.setVisibility(View.VISIBLE);
-                mCardAnswerTextView.setText(deck.get(i).getAnswer());
-                // Hides show answer button
-                mShowCardButton.setVisibility(View.GONE);
-                //Shows next card button
-                mNextCard.setVisibility(View.VISIBLE);
-                i++;
+                if(mToggleButton.isChecked()){
+                    //Shows answer
+                    mCardAnswerTextView.setVisibility(View.VISIBLE);
+                    mCardAnswerTextView.setText(shuffleDeck.get(i).getAnswer());
+                    // Hides show answer button
+                    mShowCardButton.setVisibility(View.GONE);
+                    //Shows next card button
+                    mNextCard.setVisibility(View.VISIBLE);
+                    i++;
+                }else{
+                    //Shows answer
+                    mCardAnswerTextView.setVisibility(View.VISIBLE);
+                    mCardAnswerTextView.setText(deck.get(i).getAnswer());
+                    // Hides show answer button
+                    mShowCardButton.setVisibility(View.GONE);
+                    //Shows next card button
+                    mNextCard.setVisibility(View.VISIBLE);
+                    i++;
+                }
             }
         };
 
@@ -61,21 +78,81 @@ public class CardView extends AppCompatActivity {
                 if(i == deck.size()){
                     i = 0;
                 }
-                //Hides Answer text view
-                mCardAnswerTextView.setVisibility(View.GONE);
-                //Set text of next question
-                mCardQuestionTextView.setText(deck.get(i).getQuestion());
-                //Shows next question
-                mCardQuestionTextView.setVisibility(View.VISIBLE);
-                // Shows show answer button
-                mShowCardButton.setVisibility(View.VISIBLE);
-                //Hides next card button
-                mNextCard.setVisibility(View.GONE);
+
+                if(mToggleButton.isChecked()){
+                    //Hides Answer text view
+                    mCardAnswerTextView.setVisibility(View.GONE);
+                    //Set text of next question
+                    mCardQuestionTextView.setText(shuffleDeck.get(i).getQuestion());
+                    //Shows next question
+                    mCardQuestionTextView.setVisibility(View.VISIBLE);
+                    // Shows show answer button
+                    mShowCardButton.setVisibility(View.VISIBLE);
+                    //Hides next card button
+                    mNextCard.setVisibility(View.GONE);
+                }else{
+                    //Hides Answer text view
+                    mCardAnswerTextView.setVisibility(View.GONE);
+                    //Set text of next question
+                    mCardQuestionTextView.setText(deck.get(i).getQuestion());
+                    //Shows next question
+                    mCardQuestionTextView.setVisibility(View.VISIBLE);
+                    // Shows show answer button
+                    mShowCardButton.setVisibility(View.VISIBLE);
+                    //Hides next card button
+                    mNextCard.setVisibility(View.GONE);
+                }
+
             }
         };
 
         mShowCardButton.setOnClickListener(showAnswer);
         mNextCard.setOnClickListener(nextCard);
+
+        //Code for toggling toggle
+        mToggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                //Reset i to 0 so that the deck restarts
+                i=0;
+
+                if (!isChecked && mCardAnswerTextView.getVisibility() == View.VISIBLE) {
+                    // If it is not checked and the answer is visible,
+                    // show the beginning of the non-shuffled deck and hide the answer
+
+                    mCardQuestionTextView.setText(deck.get(0).getQuestion());
+                    mCardAnswerTextView.setVisibility(View.GONE);
+                    // Shows show answer button
+                    mShowCardButton.setVisibility(View.VISIBLE);
+                    //Hides next card button
+                    mNextCard.setVisibility(View.GONE);
+
+                } else if (!isChecked && mCardAnswerTextView.getVisibility() == View.GONE) {
+                    // If it is not checked and the answer is not visible,
+                    // show the beginning of the non-shuffled deck
+
+                    mCardQuestionTextView.setText(deck.get(0).getQuestion());
+
+                }else if(isChecked && mCardAnswerTextView.getVisibility() == View.VISIBLE){
+                    // If it is checked and the answer is visible, shuffle the deck,
+                    // show the beginning of the shuffled deck and hide the answer
+
+                    Collections.shuffle(shuffleDeck);
+                    mCardQuestionTextView.setText(shuffleDeck.get(0).getQuestion());
+                    mCardAnswerTextView.setVisibility(View.GONE);
+                    // Shows show answer button
+                    mShowCardButton.setVisibility(View.VISIBLE);
+                    //Hides next card button
+                    mNextCard.setVisibility(View.GONE);
+
+                }else if(isChecked && mCardAnswerTextView.getVisibility() == View.GONE){
+                    // If it is checked and the answer is not visible, shuffle the deck,
+                    // show the beginning of the shuffled deck
+
+                    Collections.shuffle(shuffleDeck);
+                    mCardQuestionTextView.setText(shuffleDeck.get(0).getQuestion());
+                }
+            }
+        });
     }
 
 
