f342b4e27eb5ff817efe3438b3208c886fba10f6
Isaiah-Simon <zays1993@gmail.com>
2016-02-29 20:57:32 -0500

    Revert "Added Priority Attribute To Card Class"
    This reverts commit 93170fa3a1f009ba29c43220d90173ffd9444068.

--- TobiraFlashCards/app/src/main/java/com/isaiahsimon/tobiraflashcards/Card.java
+++ TobiraFlashCards/app/src/main/java/com/isaiahsimon/tobiraflashcards/Card.java
@@ -8,7 +8,6 @@ import java.io.Serializable;
 public class Card implements Serializable {
     private String question;
     private String answer;
-    private int priority;
 
     public Card(String question, String answer){
         this.question = question;
@@ -29,17 +28,7 @@ public class Card implements Serializable {
         return answer;
     }
 
-    public int getPriority() {
-        return  priority;
-    }
-    
-    public void setPriority(int priority) {
-        this.priority = priority;
-    }
-
     public String toString() {
         return question;
     }
-
-
 }
